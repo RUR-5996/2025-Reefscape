592bb3550f6def068883745213a2db1cd3b0f6fe
RUR <97384151+rur5996@users.noreply.github.com>
2025-01-10 15:51:51 +0100

    Add new subsystems

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Climber {
+    
+}
