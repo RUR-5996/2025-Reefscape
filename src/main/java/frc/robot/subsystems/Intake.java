5dcf1dba0d253c666ae136c2e384cbe8b9fb37c6
RUR <97384151+rur5996@users.noreply.github.com>
2025-02-23 15:33:27 +0100

    Make intake work with double solenoids

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,7 +11,8 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -23,11 +24,11 @@ public class Intake extends SubsystemBase {
     RelativeEncoder intakeEncoder;
     SparkClosedLoopController intakeController;
 
-    Solenoid intakeSolenoid;
+    DoubleSolenoid intakeSolenoid;
     PneumaticsControlModule intakeModule;
 
     IntakeState intakeState = IntakeState.EMPTY;
-    SolenoidState solenoidState = SolenoidState.FALSE; //might be needed to be set to true
+    SolenoidState solenoidState = SolenoidState.FORWARD;
 
     public Intake() {}; //for testing
 
@@ -51,18 +52,18 @@ public class Intake extends SubsystemBase {
 
         intakeModule = new PneumaticsControlModule(0);
 
-        intakeSolenoid = new Solenoid(0, PneumaticsModuleType.CTREPCM, frontPiston);
-        intakeSolenoid.set(false); //might be needed to be set to true
+        intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+        intakeSolenoid.set(DoubleSolenoid.Value.kForward);
     }
 
     public Command toggleSolenoid() {
         return Commands.runOnce(() -> {
-            if (solenoidState == SolenoidState.FALSE) {
-                intakeSolenoid.set(true);
-                solenoidState = SolenoidState.TRUE;
+            if (solenoidState == SolenoidState.FORWARD) {
+                intakeSolenoid.set(Value.kReverse);
+                solenoidState = SolenoidState.REVERSE;
             } else {
-                intakeSolenoid.set(false);
-                solenoidState = SolenoidState.FALSE;
+                intakeSolenoid.set(Value.kForward);
+                solenoidState = SolenoidState.FORWARD;
             }
         });
     }
@@ -70,7 +71,7 @@ public class Intake extends SubsystemBase {
     public Command intake() {
         return Commands.run(() -> {
             intakeMotor.set(.5);
-        }); //TODO
+        });
     }
 
     public String getIntakeState() {
@@ -88,8 +89,8 @@ public class Intake extends SubsystemBase {
     }
 
     private enum SolenoidState {
-        TRUE,
-        FALSE,
+        FORWARD,
+        REVERSE,
         ERROR,
     }
 }
