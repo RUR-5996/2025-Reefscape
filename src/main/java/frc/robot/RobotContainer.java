8d062e7ad73fba7e1823c4247178ba250309dd05
Agata Ferianc <agata@ferianc.com>
2025-03-08 14:56:39 +0100

    added position reset at reef

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -99,6 +99,8 @@ public class RobotContainer {
 
           xBox.povLeft().onTrue(VISION.reefMove("left"));
           xBox.povRight().onTrue(VISION.reefMove("right"));
+
+          xBox.back().onTrue(SWERVE.resetAtReef(VISION)); // tbh idk which button this is
       }
 
       private void loadPaths() {
